20572accfe15850a4623a1b06fb9bebe8af25cc2
Doug Admin <dougadmin28@DatafileTechnologies.local>
2019-06-17 15:47:52 -0500

    started writing the orm.js file, pasted default connection.js file

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+//TODO create global function to store burgerData into an array//
+
+function dataToSQL(data) {
+    //! I took this from the solution file, not sure how I would have figured this out without it
+    //TODO Create blank array to hold the data from the querries
+    let arr = [];
+
+    //? Loop through our response AKA: data
+    for( var key in data) {
+        //TODO push() each key and data[key] into the new array
+        arr.push(key + "=" + data[key]);
+    }
+    // return the new array as string
+    return arr.toString();
+}

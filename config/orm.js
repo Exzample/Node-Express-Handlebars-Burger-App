942cb0e4aa77526b48287e09a62f2ee5ee043b42
Doug Admin <dougadmin28@DatafileTechnologies.local>
2019-06-18 11:18:00 -0500

    fixed connection.js, and completed the orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,19 @@
+const connnection = require("./connection.js");
+
+
+//! Took this from the solution file//
+function printQuestionMarks(num) {
+    //TODO create empty array to store "?"
+    var arr = [];
+    //? Looping through the numArr we created so we can push "?" for each index position//
+    for ( i = 0; i < num; i++) {
+        //? Why are you pushing a "?" in the array?//
+        arr.push("?");
+    }
+    //Todo return arr and convert it to string
+    return arr.toString();
+}
+
 //TODO create global function to store burgerData into an array//
 
 function dataToSQL(data) {
@@ -15,3 +31,50 @@ function dataToSQL(data) {
 }
 
 // TODO create an "orm" object that holds the query sentax for ALL, CREATE & UPDATE//
+let orm = {
+    all: (tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query( queryString, (err, res) => {
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    } ,
+    create: (table, cols, vals, cb) => {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " ("
+        queryString += cols.toString();
+        queryString += ") "
+        queryString += "VALUES ("
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        connection.query(queryString, vals, (err, res) => {
+            if (err) {
+                throw (err);
+            }
+            cb(res);
+        });
+    },
+    
+    update: (table, objColVals, condition, cb ) => {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET "
+        queryString += dataToSQL(objColVals);
+        queryString += " WHERE "
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    };
+};
+
+module.exports = orm;
